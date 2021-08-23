5a7747c17b6b22b747a4b96b1510936d39263d3f
Hemit99123 <hemitpatel@computer4u.com>
2021-08-23 11:09:58 -0400

    'msg'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import PDF417 from "pdf417-generator";
+import styled from "styled-components";
+import { useEffect, useRef, useState } from "react";
 
-function App() {
+const BarcodeCanvas = styled.canvas`
+  width: 100%;
+`;
+
+const CanvasContainer = styled.div`
+  width: 200px;
+  border-radius: 3px;
+  padding: 10px;
+  background-color: #fff;
+`;
+
+export default function App() {
+  const [value, setValue] = useState('')
+  const canvas = useRef(null);
+
+  function download(){
+    var canvas = document.getElementById("canvas");
+    var url = canvas.toDataURL("image/png");
+    var link = document.createElement('a');
+    link.download = 'filename.png';
+    link.href = url;
+    link.click();
+  }
+  
+  useEffect(() => {
+    if (canvas.current) {
+      PDF417.draw(value, canvas.current, 3);
+    }
+  }, [canvas, value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{value}</h1>
+      <input placeholder="Enter Text Here" type='text' onChange={e => setValue(e.target.value)}/>
+
+      <CanvasContainer>
+        <BarcodeCanvas ref={canvas} id="canvas"/>
+      </CanvasContainer>
+      <button onClick={download}>download</button>
     </div>
   );
 }
-
-export default App;
